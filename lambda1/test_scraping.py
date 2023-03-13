d938b8aab28b57ec07bdf6b26a2001a99591c5e2
Ubuntu <herlendy.sanchez11@gmail>
2023-03-13 02:36:11 +0000

    Actualizacion pruebas unitarias

--- lambda1/test_scraping.py
+++ lambda1/test_scraping.py
@@ -2,7 +2,17 @@ from apps import datos
 import json
 
 
-def test_datos(mocker):
+def test_datos1(mocker):
+    url = "https://api.fincaraiz.com.co/document/api/1.0/listing/search"
+    mock = mocker.patch('requests.post')
+    mock.return_value = '{"casa":{"area":35,"banhos":3,"price":320000000}}'
+    respuesta = json.loads(datos(url))
+    assert respuesta["casa"]["area"] == 35
+    assert respuesta["casa"]["banhos"] == 3
+    assert respuesta["casa"]["price"] == 320000000
+
+
+def test_datos2(mocker):
     url = "https://api.fincaraiz.com.co/document/api/1.0/listing/search"
 
     mocker.patch('requests.post', return_value='23')
@@ -11,9 +21,11 @@ def test_datos(mocker):
     assert isinstance(respuesta, str)
     assert respuesta[0] == '2'
     assert int(respuesta[0]) + int(respuesta[1]) == 5
+    
 
-    mock = mocker.patch('requests.post')
-    mock.return_value = '{"casa":{"area":35,"banhos":3,"price":320000000}}'
-    respuesta = json.loads(datos(url))
-    print(respuesta)
-    assert respuesta["casa"]["area"] == 35
+def test_datos3():
+    url ="https://api.fincaraiz.com.co/document/api/1.0/listing/search"
+    assert len(url) == 60
+    
+    
+    
